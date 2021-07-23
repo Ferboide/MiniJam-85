7b0662c834c8d6c4629c491ac8c233df3ad446af
Bofumaga <rodricid2002@hotmail.com>
2021-07-23 10:02:47 +0200

    dame dane

---
+++ Mini Jam 85/Assets/Scripts_Bofumaga/Object.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+
+    public string[] objetos= {"Ingrediente1","Ingrediente2","Ingrediente3" };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+  
+}
