5d8b94e374f10d0a4a6a63532cb44e57e11e60a3
JorgePons <56955656+JorgePons@users.noreply.github.com>
2021-07-24 14:57:04 +0200

    AnimacionesPersonajeYHumo

---
+++ Mini Jam 85/Assets/TimerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+
+    public float time;
+    public Text timerText;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        timerText.text = time.ToString("F2");
+        if (time <= 0)
+        {
+            TimeUp();
+        }
+    }
+    void TimeUp()
+    {
+        Debug.Log("TIME UP");
+        this.enabled = false;
+    }
+}
