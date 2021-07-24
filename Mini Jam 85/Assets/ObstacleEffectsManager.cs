cc25ecc4bf2e52687370fb0e9bc5e8c8cfe38a1c
JorgePons <56955656+JorgePons@users.noreply.github.com>
2021-07-24 18:16:22 +0200

    MenuStuff

---
+++ Mini Jam 85/Assets/ObstacleEffectsManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObstacleEffectsManager : MonoBehaviour
+{
+
+    public float time;
+    public Text timerText;
+    public float effectChangeTime;
+    public int randomNumber;
+
+    public GameObject smoke;
+    public bool transparency = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= effectChangeTime)
+        {
+            randomNumber = Random.Range(1, 4);
+            switch (randomNumber)
+            {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+            }
+        }
+
+    }
+
+    public void ActivateSmoke()
+    {
+        smoke.SetActive(true);
+    }
+
+
+
+}
