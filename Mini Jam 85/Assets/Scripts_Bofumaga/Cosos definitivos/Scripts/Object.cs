c8e1ffe6524a8ade77be1afeee1aa6d7f9d7b872
Bofumaga <rodricid2002@hotmail.com>
2021-07-24 12:42:07 +0200

    washu

---
+++ Mini Jam 85/Assets/Scripts_Bofumaga/Cosos definitivos/Scripts/Object.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+    public int ID;
+    
+    public string[] ingredients = { "Ingrediente1", "Ingrediente2", "Ingrediente3" };
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Caldero"))
+        {
+            Destroy(gameObject);
+            if (Cauldron.instance.recipe[0] == this.ingredients[ID])
+            {
+                Cauldron.instance.recipe_completed.Add(this.ingredients[ID]);
+                Cauldron.instance.recipe.RemoveAt(0);
+                Cauldron.instance.borrado = true;
+            }
+
+        }
+    }
+
+
+}
